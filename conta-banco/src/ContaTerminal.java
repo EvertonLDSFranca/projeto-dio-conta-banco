85373083728e9adb97870a5af8330f046ccad596
Everton França <evertonluiz5470@gmail.com>
2024-05-15 23:19:20 -0300

    conta banco semi acabada

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -6,7 +6,7 @@ public class ContaTerminal {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.ENGLISH);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Olá! Por favor, digite o número da sua conta:");
         int conta = scanner.nextInt();
@@ -20,10 +20,7 @@ public class ContaTerminal {
         System.out.println("Por favor " + nomeCliente + ", digite o seu saldo:");
         double saldo = scanner.nextDouble();
 
-        System.out.println("");
-
-    
-
+    System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + ", e seu saldo " + saldo + " já está disponivel para saque.");
 
 
     }
